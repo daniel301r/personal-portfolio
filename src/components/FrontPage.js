e5bb001cd40b018a89b74cc3c0ff27d6fbc903e8
daniel301r <daniel.munns247@gmail.com>
2019-11-05 20:13:09 +0000

    small updates to front page

--- src/components/FrontPage.js
+++ src/components/FrontPage.js
@@ -4,11 +4,18 @@ import { Link } from 'react-router-dom'
 const FrontPage = () => {
     return (
         <div id="front-page">
+            {/* make the tags a different font - curvise */}
+            <span className="tags top-tags">{"<body>"}</span>
             <div className="text-area">
                 <h1>Hi,<br/>I'm Dan,<br/>web developer.</h1>
                 <h4>Full-Stack Developer / React / Javascript</h4>
                 <Link to="/portfolio">Portfolio</Link>
             </div>
+            <span className="tags bottom-tags">
+                {"</body>"}
+                <br/>
+                {"</html>"}
+            </span>
         </div>
     );
 };
